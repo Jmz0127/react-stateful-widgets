df37248687e63c41c61d77767ff8958ba665ed82
Jmz0127 <jmezz1@icloud.com>
2021-11-01 17:36:43 -0400

    finished mood

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -35,29 +35,32 @@ const happyMood = 'Quite happy!';
 const sadMood = 'Rather sad';
 
 export default function Moods() {
-  const [mood, setMood] = useState(sadMood)
+  const [mood, setMood] = useState(initialMood)
   /* STEP 1 */
 
   const makeHappy = () => {
+    setMood(happyMood)
     /* STEP 4 */
   };
   const makeSad = () => {
+    setMood(sadMood)
     /* STEP 5 */
   };
   const reset = () => {
+    setMood(initialMood)
     /* STEP 6 */
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: mood === makeHappy ? 'royal blue' : 'crimson', /* STEP 2 */
+    color: mood === happyMood ? 'blue' : 'crimson', /* STEP 2 */
   };
 
   return (
     <div className='widget-moods container'>
       <h2>Moods</h2>
-      <div id='mood' style={style}>{mood}</div> {/* STEP 3 */}
+      <div id='mood' style={style}>{mood}</div> 
       <div>
         <button id='makeHappy' onClick={makeHappy}>Make Happy</button>
         <button id='makeSad' onClick={makeSad}>Make Sad</button>
